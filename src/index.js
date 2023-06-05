a687f75af22143f7924448490aa54ad63c92fd8b
W-Novytskyi <113388494+W-Novytskyi@users.noreply.github.com>
2023-06-05 16:30:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/test">
+    <BrowserRouter basename="/test-goit">
       <App />
     </BrowserRouter>
   </React.StrictMode>
